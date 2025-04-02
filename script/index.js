f28ac125dd0f5017d1644b17ca17455c225d90d7
Erick <122824991+ErickLarssen@users.noreply.github.com>
2025-04-02 17:17:25 -0300

    Primeiro commit

--- script/index.js
+++ script/index.js
@@ -1,8 +1,38 @@
 const members = [
-    { name: 'Peter Quill', id: 'peter' },
-    { name: 'Gamora Titan', id: 'gamora' },
-    { name: 'Drax o Destruidor', id: 'drax' },
-    { name: 'Mantis', id: 'mantis' },
-    { name: 'Rocket Raccoon', id: 'rocket' },
-    { name: 'I am Groot', id: 'groot' },
-  ]
+  { name: 'Peter Quill', id: 'peter' },
+  { name: 'Gamora Titan', id: 'gamora' },
+  { name: 'Drax o Destruidor', id: 'drax' },
+  { name: 'Mantis', id: 'mantis' },
+  { name: 'Rocket Raccoon', id: 'rocket' },
+  { name: 'I am Groot', id: 'groot' },
+]
+
+let activeMember = 0
+const images = document.getElementById('images')
+const menu = document.getElementById('menu')
+
+function changeStatusButtons(){
+  const prev = document.getElementById('button__prev')
+  const isFirst = activeMember === 0
+  prev.disabled = isFirst
+
+  const next = document.getElementById('button__next')
+  const isLast = activeMember === members.length - 1
+  next.disabled = isLast
+}
+
+function navigationMember(direction){
+  activeMember = activeMember + direction
+
+  const member = members[activeMember]
+
+  console.log(activeMember, member);
+
+  images.style.transform = `translateY(${100 * activeMember}vh)`
+
+  changeStatusButtons()
+}
+
+function changeMenu(){
+  menu.classList.toggle('active')
+}
